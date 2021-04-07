db2086be862d21441a81ccc4adbdd5d1b59b1655
stephanieduncan <stephanieduncan@outlook.com>
2021-04-07 22:38:34 +0100

    creating second tab

--- home_energy/server.R
+++ home_energy/server.R
@@ -1,10 +1,81 @@
 server <- function(input, output) {
+  
+  ##################First tab content
+  overview_output <- reactive({
+    home_energy %>% 
+      filter(year == input$year)
     
-    ##################First tab content
+  })
+  
+  output$co2_pfa_overview <- renderPlot({
+    
+    overview_output() %>%
+      group_by(ca_name, year) %>% 
+      summarise(mean_co2_pfa = round(mean(co2_emissions_per_floor_area), digits = 2),
+                mean_primary_energy = round(mean(primary_energy), digits = 2),
+                mean_current_emissions = round(mean(current_emissions_t_co2_yr), digits = 2)) %>% 
+      ggplot() +
+      aes(x = ca_name, y = mean_co2_pfa) +
+      geom_col(fill = "dark green") +
+      theme_minimal() +
+      labs(
+        y = "Mean CO2 Emissions
+        (kg CO2/m^2/Year)",
+        x = "Local Authority Area",
+        title = "Mean CO2 Emissions per Current Floor Area of Homes",
+        subtitle = "2012 - 2020"
+      ) +
+      theme(axis.text.x = element_text(angle=45,hjust=1)) 
+    
+  })
+  
+  output$primary_overview <- renderPlot({
+    
+    overview_output() %>%
+      group_by(ca_name, year) %>% 
+      summarise(mean_co2_pfa = round(mean(co2_emissions_per_floor_area), digits = 2),
+                mean_primary_energy = round(mean(primary_energy), digits = 2),
+                mean_current_emissions = round(mean(current_emissions_t_co2_yr), digits = 2)) %>% 
+      ggplot() +
+      aes(x = ca_name, y = mean_primary_energy) +
+      geom_col(fill = "dark green") +
+      theme_minimal() +
+      labs(
+        y = "Mean Primary Energy",
+        x = "Local Authority Area",
+        title = "Mean Primary Energy of Homes",
+        subtitle = "2012 - 2020"
+      ) +
+      theme(axis.text.x = element_text(angle=45,hjust=1)) 
+    
+  })
+  
+  
+  output$current_overview <- renderPlot({
+    
+    overview_output() %>%
+      group_by(ca_name, year) %>% 
+      summarise(mean_co2_pfa = round(mean(co2_emissions_per_floor_area), digits = 2),
+                mean_primary_energy = round(mean(primary_energy), digits = 2),
+                mean_current_emissions = round(mean(current_emissions_t_co2_yr), digits = 2)) %>% 
+      ggplot() +
+      aes(x = ca_name, y = mean_current_emissions) +
+      geom_col(fill = "dark green") +
+      theme_minimal() +
+      labs(
+        y = "Mean Current Emissions",
+        x = "Local Authority Area",
+        title = "Mean Current Emissions of Homes",
+        subtitle = "2012 - 2020"
+      ) +
+      theme(axis.text.x = element_text(angle=45,hjust=1)) 
+    
+  })
+    
+    ##################Second tab content
     home_energy_output <- reactive({
         home_energy %>% 
-            filter(ca_name == input$ca_name,
-                   year == input$year)
+            filter(ca_name == input$ca_name)
         
     })
     
@@ -16,17 +87,63 @@ server <- function(input, output) {
                       mean_primary_energy = round(mean(primary_energy), digits = 2),
                       mean_current_emissions = round(mean(current_emissions_t_co2_yr), digits = 2)) %>% 
             ggplot() +
-            aes(x = mean_co2_pfa, y = ca_name) +
-            geom_col(fill = "dark green") +
+            aes(x = year, y = mean_co2_pfa) +
+            geom_line(fill = "dark green") +
+        geom_point() +
             theme_minimal() +
             labs(
-                y = "Council Area",
-                x = "Mean CO2 Emissions Per Current Floor Area (kg CO2/m^2/Year)",
-                title = "Mean CO2 Emissions Current Floor Area of Homes by Council Area",
+                y = "Mean CO2 Emissions (kg CO2/m^2/Year)",
+                x = "Year",
+                title = "Mean CO2 Emissions per Current Floor Area of Homes",
                 subtitle = "2012 - 2020"
             ) +
             theme(axis.text.x = element_text(angle=45,hjust=1)) 
         
+    })
+    
+    output$primary_energy_output <- renderPlot({
+      
+      home_energy_output() %>%
+        group_by(ca_name, year) %>% 
+        summarise(mean_co2_pfa = round(mean(co2_emissions_per_floor_area), digits = 2),
+                  mean_primary_energy = round(mean(primary_energy), digits = 2),
+                  mean_current_emissions = round(mean(current_emissions_t_co2_yr), digits = 2)) %>% 
+        ggplot() +
+        aes(x = year, y = mean_primary_energy) +
+        geom_line(fill = "dark green") +
+        geom_point() +
+        theme_minimal() +
+        labs(
+          y = "Mean Primary Energy",
+          x = "Year",
+          title = "Mean Primary Energy of Homes",
+          subtitle = "2012 - 2020"
+        ) +
+        theme(axis.text.x = element_text(angle=45,hjust=1)) 
+      
+    })
+    
+    
+    output$current_emissions_output <- renderPlot({
+      
+      home_energy_output() %>%
+        group_by(ca_name, year) %>% 
+        summarise(mean_co2_pfa = round(mean(co2_emissions_per_floor_area), digits = 2),
+                  mean_primary_energy = round(mean(primary_energy), digits = 2),
+                  mean_current_emissions = round(mean(current_emissions_t_co2_yr), digits = 2)) %>% 
+        ggplot() +
+        aes(x = year, y = mean_current_emissions) +
+        geom_line(fill = "dark green") +
+        geom_point() +
+        theme_minimal() +
+        labs(
+          y = "Mean Current Emissions",
+          x = "Year",
+          title = "Mean Current Emissions of Homes",
+          subtitle = "2012 - 2020"
+        ) +
+        theme(axis.text.x = element_text(angle=45,hjust=1)) 
+      
     })
     
     
--- home_energy/ui.R
+++ home_energy/ui.R
@@ -12,86 +12,138 @@
 source("global.R")
 
 ui <- dashboardPage(
-    
-    dashboardHeader(title = "Home Energy"),
-    ## Sidebar content
-    dashboardSidebar(
-        sidebarMenu(
-            menuItem("Home Energy by Area", tabName = "home_energy_area"),
-            menuItem("About", tabName = "about")
-        )
-    ),
-    
-    ## Body content
-    dashboardBody(
-        shinyDashboardThemes(theme = "blue_gradient"),
-        tabItems(
-            
-            
-            
-            # home energy content
-            tabItem(tabName = "home_energy_area",
-                    h2("Home Energy in Scotland"),
-                    
-                    fluidRow(
-                        box(width = 12,
-                            background = "light-blue",
-                            column(width = 6, 
-                                   selectInput(inputId = "ca_name",
-                                               label = "Area",
-                                               choices = sort(unique(home_energy$ca_name)),
-                                               selected = "Aberdeen City"
-                                   )
-                            ),
-                            
-                            column(width = 6, 
-                                   align = "center",
-                                   radioButtons(inputId = "year",
-                                                label = "Year",
-                                                choices = unique(home_energy$year),
-                                                selected = "2020",
-                                                inline = TRUE) 
-                            )
-                        )
-                    ),
-                    
-                    fluidRow(
-                        
-                        box(
-                            title = "Home Energy by Area", 
-                            solidHeader = TRUE,
-                            status = "primary",
-                            plotOutput("home_energy_output", height = 250)
-                        )
-                    )
-            ), 
-            
-            
-           
-            # About tab content
-            tabItem(tabName = "about",
-                    h1("About"),
-                    h3("Author:", tags$a(href = "https://www.linkedin.com/in/stephanie-mpd/",
-                                         "Stephanie Duncan")),
-                    "This interactive dashboard gives insights and trends on home energy in Scotland between 2012 - 2020.",
-                    br(),
-                    "The code I wrote to produce this dashboard can be found on my ", tags$a(href = "https://github.com/stephanieduncan/climate_challenge_cop26/", "Github Repository"),
-                    br(),
-                    "The data used to carry out analysis for this dashboard is open source and can be found in the links below.",
-                    br(),
-                    br(),
-                    fluidRow(
-                        box(title = "Data Sources", solidHeader = TRUE, status = "primary",
-                            h5(strong("Statistics.gov.scot")),
-                            
-                            tags$a(href="https://statistics.gov.scot/data/domestic-energy-performance-certificates", 
-                                   "Home Energy Performance Certificates Data"),
-                            br(),
-                            tags$a(href="https://www.opendata.nhs.scot/es_AR/dataset/geography-codes-and-labels/resource/e92d19d4-ced7-40c8-b628-e28e4528fc41", 
-                                   "Geography Local Authority Area Labels Lookup")
-                        )
-                    )
-            )
-        )
+  
+  dashboardHeader(title = "Home Energy"),
+  ## Sidebar content
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Overview", tabName = "scotland_overview"),
+      menuItem("Home Energy by Area", tabName = "home_energy_area"),
+      menuItem("About", tabName = "about")
     )
+  ),
+  
+  ## Body content
+  dashboardBody(
+    shinyDashboardThemes(theme = "poor_mans_flatly"),
+    tabItems(
+      
+      
+      # overview of Scotland
+      tabItem(tabName = "scotland_overview",
+              h2("How is Scotland doing?"),
+              
+              fluidRow(
+                box(width = 12,
+                    background = "light-blue",
+                    column(width = 6, 
+                           selectInput(inputId = "year",
+                                       label = "Year",
+                                       choices = sort(unique(home_energy$year)),
+                                       selected = "2020"
+                           )
+                    )
+                )
+              ),
+              
+              fluidRow(
+                
+                box(
+                  title = "CO2 Emissions in homes per Current Floor Area",
+                  status = "primary",
+                  solidHeader = TRUE,
+                  plotOutput("co2_pfa_overview", height = 250)
+                ),
+                
+                box(
+                  title = "Primary Energy",
+                  status = "primary",
+                  solidHeader = TRUE,
+                  plotOutput("primary_overview", height = 250)
+                ),
+                
+                box(
+                  title = "Current Emissions",
+                  status = "primary",
+                  solidHeader = TRUE,
+                  plotOutput("current_overview", height = 250)
+                ),
+                
+              )
+              
+              
+              ),
+      
+      
+      
+      # home energy content
+      tabItem(tabName = "home_energy_area",
+              h2("Temporal"),
+              
+              fluidRow(
+                box(width = 12,
+                    background = "light-blue",
+                    column(width = 6, 
+                           selectInput(inputId = "ca_name",
+                                       label = "Area",
+                                       choices = sort(unique(home_energy$ca_name)),
+                                       selected = "Aberdeen City"
+                           )
+                    )
+              )
+              ),
+              
+              fluidRow(
+                
+                box(
+                  title = "Home Energy by Area", 
+                  solidHeader = TRUE,
+                  status = "primary",
+                  plotOutput("home_energy_output", height = 250)
+              ),
+                
+                box(
+                  title = "Primary Energy by Area", 
+                  solidHeader = TRUE,
+                  status = "primary",
+                  plotOutput("primary_energy_output", height = 250)
+                ),
+        
+        box(
+          title = "Current Emissions by Area", 
+          solidHeader = TRUE,
+          status = "primary",
+          plotOutput("current_emissions_output", height = 250)
+        )
+              )
+    ), 
+      
+      
+      
+      # About tab content
+      tabItem(tabName = "about",
+              h1("About"),
+              h3("Author:", tags$a(href = "https://www.linkedin.com/in/stephanie-mpd/",
+                                   "Stephanie Duncan")),
+              "This interactive dashboard gives insights and trends on home energy in Scotland between 2012 - 2020.",
+              br(),
+              "The code I wrote to produce this dashboard can be found on my ", tags$a(href = "https://github.com/stephanieduncan/climate_challenge_cop26/", "Github Repository"),
+              br(),
+              "The data used to carry out analysis for this dashboard is open source and can be found in the links below.",
+              br(),
+              br(),
+              fluidRow(
+                box(title = "Data Sources", solidHeader = TRUE, status = "primary",
+                    h5(strong("Statistics.gov.scot")),
+                    
+                    tags$a(href="https://statistics.gov.scot/data/domestic-energy-performance-certificates", 
+                           "Home Energy Performance Certificates Data"),
+                    br(),
+                    tags$a(href="https://www.opendata.nhs.scot/es_AR/dataset/geography-codes-and-labels/resource/e92d19d4-ced7-40c8-b628-e28e4528fc41", 
+                           "Geography Local Authority Area Labels Lookup")
+                )
+              )
+      )
+    )
+  )
 )
